c567130d7979afde501064fa95f2104dd1d36f9d
Sandesh Khanal <khanalsandesh66@gmail.com>
2022-03-22 11:26:49 +0545

    Program to Find age and weight in Lbs

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-a = 10
-b = 10
-c = a * b
-print(c)
+#####################################################################################################################
+
+# Wap to Find your current Age
+
+# birth_year = input("Enter Your Birth Year ")
+# current_year = input("Enter the current year hue")
+# age = int(current_year) - int(birth_year)   #this is called type Casting /
+# print(age)
+
+######################################################################################################################
+
+# WAP to Change the User Weight in KG
+
+weight = input("Enter your Weight in KG:")
+lbs = int(weight)*2.204
+print("your weight in pound is :", + lbs)
