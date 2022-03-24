9905511b7e57e3c8ca5e908e72e32bfb0ccabcaf
Sandesh Khanal <khanalsandesh66@gmail.com>
2022-03-24 11:28:55 +0545

    Lerning Arithmetic Operations

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-#####################################################################################################################
+# name = input("What is Your Name ?")
+# print(name)
+ #####################################################################################################################
 
 # Wap to Find your current Age
 
@@ -6,11 +8,86 @@
 # current_year = input("Enter the current year hue")
 # age = int(current_year) - int(birth_year)   #this is called type Casting /
 # print(age)
+# print(f'His age is {age}')
 
 ######################################################################################################################
 
 # WAP to Change the User Weight in KG
 
-weight = input("Enter your Weight in KG:")
-lbs = int(weight)*2.204
-print("your weight in pound is :", + lbs)
+# weight = input("Enter your Weight in KG:")
+# weightInP = int(weight)*2.204
+# print("your weight in pound is :",weightInP,'lbs')
+# print(f'your weight in pound is :{weightInP}lbs')
+#
+
+ #####################################################################################################################
+
+ # WAP to Print in Different Line
+
+# Course='''
+# Fundamental Of React
+# JavaScript
+# JSX
+# Props
+# States
+# Complex States'''
+#
+# print(Course)
+
+######################################################################################################################
+
+# WAP to Print in Lower and upper Case
+
+
+# Course='''
+# Fundamental Of React
+# JavaScript
+# JSX
+# Props
+# States
+# Complex States'''
+#
+# print(Course.upper())
+# print(Course.lower())
+# print(Course.title())
+
+######################################################################################################################
+
+# WAP to Print in Lower and upper Case
+
+# Course="Its me Sandesh Khanal"
+#
+# print(Course[0])
+# print(Course[-1])
+# print(Course[3:8])
+# print(Course[3:]) # Print Without some letters from first
+# print(Course[:2]) # print some letters
+# print(Course.find('a'))
+# print(Course.replace('Sandesh','MrSandesh'))
+
+##################################################################################################################
+
+# Arthemetic operation
+
+print(8+3)
+print(9-8)
+print(2*200)
+print(2/3)
+print(5%2)
+print(2**3)
+print(5//2)
+print(7//3)
+
+x = 10
+x = x + 1
+x += 1
+print (x)
+
+print (2+3*5-4)
+print((2*3)*5-4/2**2)
+
+# Braket >> ** >> division >> multiply >>plus >> minus
+
+##################################################################################################################
+
+
