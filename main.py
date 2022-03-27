9000df22aaf7146f5c62046d56425ac03dd1e41a
Sandesh Khanal <khanalsandesh66@gmail.com>
2022-03-27 23:44:41 +0545

    Learning to use WHILE loop

--- main.py
+++ main.py
@@ -105,12 +105,70 @@
 
 ##################################################################################################################
 
-a = 10
-b = 20
-c = input("Enter the number")
-if int(c)>a and int(c)<b:
-    print("Number is  valid ")
-else:
-    print("invalid")
+# a = 10
+# b = 20
+# c = input("Enter the number")
+# if int(c)>a and int(c)<b:
+#     print("Number is  valid ")
+# else:
+#     print("invalid")
 
+
+##################################################################################################################
+
+ # Program to enter the 8 digit username and password
+
+# username = input("Enter your username here:")
+# if username == 'sandesh':
+#     print('Enter your password:')
+# else:
+#     print('Only our reputed manager Mr Sandesh khanal can login')
+#     exit()
+#
+# password = input()
+# if len(password)<8:
+#     print('The password must be at least 8 characters')
+# elif len(password)>10:
+#     print('The password must below 10 characters')
+# else:
+#     exit("""Your data is hacked
+#     Contact 9812322590 to get your data back  """)
+
+
+##################################################################################################################
+
+# weight = input('Please enter your weight:')
+# final_weight= input('Please enter 1 to get your weight in kg and Enter 0 to get your weight in Pound ')
+# if(int(final_weight)==1):
+#     print('your weight  in pound  is ', int(weight) * 2.204)
+# elif(int(final_weight==0)):
+#     print('your weight in kg is ', int(weight)/2.204)
+# else:
+#     print('Invalid input ')
+
+##################################################################################################################
+
+
+# i = 7
+# while i < 5:
+#     print(i)
+#     i+=1
+# exit('Nasa is hacked by ME')
+
+
+##################################################################################################################
+secret_number = 3
+guess_limit = 3
+guess_count = 0
+
+while guess_count < guess_limit:
+    guess = int(input('Enter your guess number'))
+    guess_count= guess_count +1
+    if guess == secret_number:
+        print('Congratulations you are the winner')
+        break
+    else:
+        print('Incorrect , you have', guess_limit-guess_count,'guesses left')
+else:
+    exit('You failed!!')
 
